1a03fefc26af26d634214dd7d2af92e874ba0fa1
milli Watt micro <wys925@my.utsa.edu>
2019-06-27 16:51:23 -0500

    json operations to be added

--- utils/json_crud.py
+++ utils/json_crud.py
@@ -16,13 +16,23 @@ json_string = """
 """
 
 class JsonCRUD:
+    json_file_path = ""
     def __init__(self, json_path):
         self.json_file_path = json_path
 
-    def save_json(self, dict, save_path):
+    def create(self, dict, save_path):
         with open(save_path, 'r+') as jf:
             json_dict = json.load(jf)
 
         with open(save_path, 'w+') as wjf:
             print(json_data)
             json.dump(dict, jf)
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
