ddbee84aa403e9eae4894516b53448f88baaa794
run-me <wys925@my.utsa.edu>
2019-06-27 21:06:56 -0500

    code cleanup

--- utils/kaggle_scratch.py
+++ utils/kaggle_scratch.py
@@ -1,13 +1,6 @@
 # For running inference on the TF-Hub module.
-
 import tensorflow as tf
 from model.base_model import BaseModel
-
-# For downloading the image.
-import matplotlib.pyplot as plt
-import tempfile
-from six.moves.urllib.request import urlopen
-from six import BytesIO
 
 from PIL import Image
 from PIL import ImageOps
@@ -18,27 +11,6 @@ import os
 
 # Check available GPU devices.
 print("The following GPU devices are available: %s" % tf.test.gpu_device_name())
-
-
-def display_image(image):
-    # fig = plt.figure(figsize=(20, 15))
-    plt.grid(False)
-    plt.imshow(image)
-
-
-def download_and_resize_image(url, new_width=256, new_height=256, display=False):
-    _, filename = tempfile.mkstemp(suffix=".jpg")
-    response = urlopen(url)
-    image_data = response.read()
-    image_data = BytesIO(image_data)
-    pil_image = Image.open(image_data)
-    pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
-    pil_image_rgb = pil_image.convert("RGB")
-    pil_image_rgb.save(filename, format="JPEG", quality=90)
-    print("Image downloaded to %s." % filename)
-    if display:
-        display_image(pil_image)
-    return filename
 
 
 def image_list_generator(img_dir):
@@ -67,7 +39,7 @@ def image_feeder(image_path, new_width=256, new_height=256, display=False):
     img_byte_array = io.BytesIO()
     rgb_converted.save(img_byte_array, format='PNG')
     if display:
-        display_image(rgb_converted)
+        print("implement display image not implemented now")
     return img_byte_array.getvalue()
 
 
