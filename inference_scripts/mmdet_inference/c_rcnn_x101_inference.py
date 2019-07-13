7415e988d643f7402267ccf6543b7ef2218a88fa
mash-compute <wys925@my.utsa.edu>
2019-07-13 15:17:58 -0500

    checkpoint path added

--- inference_scripts/mmdet_inference/c_rcnn_x101_inference.py
+++ inference_scripts/mmdet_inference/c_rcnn_x101_inference.py
@@ -1,7 +1,8 @@
 from det_frameworks.mmdetection.mmdet.apis import init_detector, inference_detector, show_result
 
 config_file = '../../det_frameworks/mmdetection/configs/cascade_rcnn_x101_64x4d_fpn_1x.py'
-checkpoint_file = '../../checkpoints/cascaded_rcnn_x101_64x4d/cascade_rcnn_x101_64x4d_fpn_2x_20181218-5add321e.pth'
+checkpoint_file = '../../det_frameworks/mmdetection/checkpoints/cascade_rcnn_x101_64x4d/' \
+                  'cascade_rcnn_x101_64x4d_fpn_2x_20181218-5add321e.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
