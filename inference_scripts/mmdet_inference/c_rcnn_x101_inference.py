f0aa94e4110cb80f87cc48267272759512035657
milli Watt micro <wys925@my.utsa.edu>
2019-07-15 11:58:48 -0500

    working detection script added

--- inference_scripts/mmdet_inference/c_rcnn_x101_inference.py
+++ inference_scripts/mmdet_inference/c_rcnn_x101_inference.py
@@ -1,13 +1,14 @@
 from det_frameworks.mmdetection.mmdet.apis import init_detector, inference_detector, show_result
 
 config_file = '../../det_frameworks/mmdetection/configs/cascade_rcnn_x101_64x4d_fpn_1x.py'
-checkpoint_file = '../../checkpoints/cascaded_rcnn_x101_64x4d/cascade_rcnn_x101_64x4d_fpn_2x_20181218-5add321e.pth'
+checkpoint_file = '../../det_frameworks/mmdetection/checkpoints/cascaded_rcnn_x101_64x4d/' \
+                  'cascade_rcnn_x101_64x4d_fpn_2x_20181218-5add321e.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
 # test a single image and show the results
-img = '../../test_images/4.jpg'  # or img = mmcv.imread(img), which will only load it once
+img = '../../test_images/3.jpg'  # or img = mmcv.imread(img), which will only load it once
 result = inference_detector(model, img)
 show_result(img, result, model.CLASSES)
 
