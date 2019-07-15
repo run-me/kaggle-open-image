e784ccecdca901ed889782be4c2a5b3a3ceec393
mash-compute <maheshwaran.umapathy@my.utsa.edu>
2019-07-14 20:31:12 -0500

    inference ready scripts

--- det_frameworks/README.md
+++ det_frameworks/README.md
@@ -2,5 +2,8 @@ This directory holds different detection frameworks being experimented with.
 
 ## Currently it holds
 1. mmdetection<br>
-Download mmdetection framework from github and put it inside this directory 
-follow the installation instructions
+#### Inference Instructions
+1. Download mmdetection framework from github and put it inside this directory 
+2. Follow the installation instructions for the mmdetection framework
+3. Ensure path for cuda and nvidia library framework if there is path error
+4. Run any python script in inference_scripts directory to verify installation
--- inference_scripts/mmdet_inference/c_rcnn_x101_inference.py
+++ inference_scripts/mmdet_inference/c_rcnn_x101_inference.py
@@ -1,14 +1,14 @@
 from det_frameworks.mmdetection.mmdet.apis import init_detector, inference_detector, show_result
 
-config_file = '../../det_frameworks/mmdetection/configs/cascade_rcnn_x101_64x4d_fpn_1x.py'
-checkpoint_file = '../../det_frameworks/mmdetection/checkpoints/cascade_rcnn_x101_64x4d/' \
+config_file = 'det_frameworks/mmdetection/configs/cascade_rcnn_x101_64x4d_fpn_1x.py'
+checkpoint_file = 'det_frameworks/mmdetection/checkpoints/cascade_rcnn_x101_64x4d/' \
                   'cascade_rcnn_x101_64x4d_fpn_2x_20181218-5add321e.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
 # test a single image and show the results
-img = '../../test_images/1.jpg'  # or img = mmcv.imread(img), which will only load it once
+img = 'test_images/1.jpg'  # or img = mmcv.imread(img), which will only load it once
 result = inference_detector(model, img)
 show_result(img, result, model.CLASSES)
 
